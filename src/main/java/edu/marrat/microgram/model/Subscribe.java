782ecb57e4b53fb8a756d299b9794b2181f18182
Marrrat <89790605+iamMarat@users.noreply.github.com>
2021-11-29 17:17:13 +0600

    add Subscribe class

---
+++ src/main/java/edu/marrat/microgram/model/Subscribe.java
@@ -0,0 +1,13 @@
+package edu.marrat.microgram.model;
+
+import java.time.LocalDate;
+
+public class Subscribe {
+    User whoSub;
+    User onWhomSub;
+    LocalDate dateSub;
+
+}
+
+
+//    Обязательные поля: Кто и c, дату события.
